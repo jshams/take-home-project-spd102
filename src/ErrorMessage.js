5852b7aadb60cef55aa0af73c9f56d690ddcff08
Jake Shams <jshams62@gmail.com>
2019-04-10 16:30:42 -0700

    added error message component

---
+++ src/ErrorMessage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Popup from 'reactjs-popup'
+
+const ErrorMessage = (props) => {
+    return (
+        <div>
+            <h1>{props.message}</h1>
+        </div>
+    )
+}
+
+export default ErrorMessage
